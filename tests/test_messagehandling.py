312ca8d025c8f2a850c01ae4d49b47f16659701e
r-k- <r-k-@users.noreply.github.com>
2020-06-08 23:22:49 +0200

    Create test_messagehandling.py
    Creating project skeleton.

---
+++ tests/test_messagehandling.py
@@ -0,0 +1,10 @@
+"""
+Test message handling functions. 
+
+"""
+from ..cryptedcommunication.messagehandling import msg_encrypt, msg_decrypt 
+
+
+def test_messages(key):
+    msg = b'A message for encryption'
+    assert msg == msg_decrypt(key, msg_encrypt(key, msg))

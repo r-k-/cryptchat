63836c366d5ee38685ec3e00a9a969cae885f2bd
r-k- <r-k-@users.noreply.github.com>
2020-06-08 23:30:14 +0200

    Update test_messagehandling.py
    PEP formatting.

--- tests/test_messagehandling.py
+++ tests/test_messagehandling.py
@@ -8,3 +8,4 @@ from ..cryptedcommunication.messagehandling import msg_encrypt, msg_decrypt
 def test_messages(key):
     msg = b'A message for encryption'
     assert msg == msg_decrypt(key, msg_encrypt(key, msg))
+

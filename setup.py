3c1654c6cdb4ae3504dc7a77bea20ea27f91c640
r-k- <r-k-@users.noreply.github.com>
2020-06-08 23:02:47 +0200

    Create setup.py
    Creating project skeleton.

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""
+Base setup script for building the module.
+
+"""
+from distutils.core import setup
+
+setup(name='cryptedcommunication',
+      version='1.0',
+      description='Encrypted communication module',
+      author='r-k-',
+      author_email='r-k-@users.github.com',
+      url='tbd',
+      packages=[],
+     )

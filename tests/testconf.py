76c6e47cdfb2799646dc2b6916e9a68c92d27507
r-k- <r-k-@users.noreply.github.com>
2020-06-08 23:27:53 +0200

    Update testconf.py
    Creating project skeleton.

--- tests/testconf.py
+++ tests/testconf.py
@@ -4,3 +4,15 @@ Pytest fixtures
 Use temp folder for running tests that after completion is deleted.
 
 """
+import pytest
+from Crypto.PublicKey import RSA
+
+
+@pytest.fixture(scope='module')
+def key():
+    yield RSA.generate(1024)
+
+
+@pytest.fixture(scope='module')
+def temp_folder():
+    pass 

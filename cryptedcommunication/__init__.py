94ca49d0f0b5f4c29873da95060e5ad6f640b75f
r-k- <r-k-@users.noreply.github.com>
2020-09-12 16:33:10 +0100

    Code cleaning.

--- cryptedcommunication/__init__.py
+++ cryptedcommunication/__init__.py
@@ -1,4 +1,4 @@
-# Externally visible functions. 
+# Externally visible functions.
 from .messagehandling import msg_crypt, msg_decrypt
 
 __all__ = ['msg_crypt', 'msg_decrypt']
--- tests/test_messagehandling.py
+++ tests/test_messagehandling.py
@@ -8,4 +8,3 @@ from ..cryptedcommunication.messagehandling import msg_decrypt, msg_crypt
 def test_messages(sample_key):
     msg = b'A message for encryption'
     assert msg == msg_decrypt(sample_key, msg_crypt(sample_key, msg))
-

c4ad10e1593fb6e696494216ddbde32a97d2e751
r-k- <r-k-@users.noreply.github.com>
2020-09-26 23:22:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,15 @@ Base setup script for building the module.
 
 """
 from distutils.core import setup
+from os.path import exists
 
+
+version_id_file = r'version_id'
+version_id = None
+if exists(version_id_file):
+	with open(version_id_file) as verf:
+		version_id = verf.readline().strip()
+	
 setup(name='cryptedcommunication',
       version='1.0',
       description='Encrypted communication module',
